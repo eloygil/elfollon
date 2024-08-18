bf74e7267f31cae22fc615c9c66ed59be02f4918
Eloy Gil Guerrero <eloy@eloygil.com>
2024-08-18 14:17:07 +0000

    Add mobile responsiveness
    Adding meta tag allowing a better appearence on mobile devices

--- cena/index.php
+++ cena/index.php
@@ -7,6 +7,7 @@ require('../../php-require/mysql-elfollon.php');
 <html>
 <head>
 <title>Cena Peña El Follón <?php echo getEventYear(); ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <style>
@@ -30,13 +31,16 @@ require('../../php-require/mysql-elfollon.php');
 .btn-whatsapp-label {
   color: #fff;
 }
+h1 {
+  text-align: center;
+}
 </style>
 </head>
 <body>
+<h1>Cena Peña "El Follón"</h1>
 <div class="container">
   <div class="row">
     <div class="col-xs-12">
-<h1>Cena Peña "El Follón"</h1>
 <b>Fecha</b>: <?php echo getEventDay(); ?> de <?php echo getEventMonthText(); ?> de <?php echo getEventYear(); ?> (<?php echo getPrintableEventTime(); ?>h.)<br>
 <b>Lugar</b>: <?php echo getEventLocation(); ?><br>
 
