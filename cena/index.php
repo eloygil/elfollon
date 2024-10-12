ff5708a22e06826ddbf22d8ebd0a8aa0255b0a81
Eloy Gil Guerrero <eloy@eloygil.com>
2024-10-12 15:21:01 +0000

    Add and set paper texture background
    Wink, wink.

--- cena/index.php
+++ cena/index.php
@@ -12,6 +12,11 @@ require('../../php-require/mysql-elfollon.php');
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <style>
+body {
+  background-image: url('img/bg.jpg');
+  background-color: rgba(255,255,255,0.6);
+  background-blend-mode: lighten;
+}
 .text-responsive {
   font-size: calc(100% + 1vw + 1vh);
 }
