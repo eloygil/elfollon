7ead6745ed214c9342b9d1f5ddf1563e9a42fc85
Eloy Gil Guerrero <eloy@eloygil.com>
2025-03-01 01:03:46 +0000

    Redesign seat allocation map (css + script)
    This change makes the distribution look better, tables are now
    shown in vertical and each group has their allocated seats
    marked in red.ºº

--- scripts/asignar.py
+++ scripts/asignar.py
@@ -9,8 +9,7 @@ import time
 DEBUG = True
 SITE_PATH = '../reserva'
 
-
-class table(object):
+class Table:
     def __init__(self, n_seats):
         self._size = n_seats
         self._used = 0
@@ -26,46 +25,9 @@ class table(object):
         self._used += number
         return seat
 
-
 def debug_print(msg, level='DEBUG'):
     if DEBUG:
         print(f"[{level}] {msg}")
-
-
-def getEventDate():
-    # The purpose of this function is reading the event date from the MySQL database
-    try:
-        date_db = getValueFromDb("SELECT fecha FROM `reserva_config` LIMIT 1")
-        return date_db if date_db else datetime.now()
-    except Exception as e:
-        print(f'Event date cannot be obtained from the database: {e}')
-        sys.exit(1)
-
-
-def getEventLimit():
-    # The purpose of this function is reading the event time limit from the MySQL database
-    return int(getValueFromDb("SELECT limite_min FROM `reserva_config` LIMIT 1"))
-
-
-def getGreenlight():
-    # It will exit in most cases, but let the execution happen if it is time to assign seats
-    try:
-        if sys.argv[1] == "force":
-            return True
-    except IndexError:
-        pass
-    dt = getEventDate() - timedelta(minutes=getEventLimit())  # The calculation happens some minutes before the event
-    now = datetime.now(tz=pytz.timezone("Europe/Madrid"))     # Adjusted to Spanish peninsular time
-    if now.year != dt.year or now.month != dt.month or now.day != dt.day or now.hour != dt.hour or now.minute != dt.minute:
-        debug_print(f"Year: {now.year} vs {dt.year}")
-        debug_print(f"Month: {now.month} vs {dt.month}")
-        debug_print(f"Day: {now.day} vs {dt.day}")
-        debug_print(f"Hour: {now.hour} vs {dt.hour}")
-        debug_print(f"Minute: {now.minute} vs {dt.minute}")
-        debug_print("Nothing to do here!")
-        exit(0)
-    time.sleep(2)
-
 
 def getMySQLCredentials():
     # The purpose of this function is avoiding yet another set of credentials hardcoded in plaintext
@@ -80,81 +42,77 @@ def getMySQLCredentials():
                     creds[key] = line.split("=")[-1].strip().replace("'", "").replace(";", "")
     return creds
 
-
 def getValueFromDb(query):
     cursor.execute(query)
     return cursor.fetchone()[0]
 
+def getEventDate():
+    try:
+        date_db = getValueFromDb("SELECT fecha FROM `reserva_config` LIMIT 1")
+        return date_db if date_db else datetime.now()
+    except Exception as e:
+        print(f'Event date cannot be obtained from the database: {e}')
+        sys.exit(1)
+
+def getEventLimit():
+    return int(getValueFromDb("SELECT limite_min FROM `reserva_config` LIMIT 1"))
+
+def getGreenlight():
+    try:
+        if sys.argv[1] == "force":
+            return True
+    except IndexError:
+        pass
+    time.sleep(1)
+    dt = getEventDate() - timedelta(minutes=getEventLimit())
+    now = datetime.now(tz=pytz.timezone("Europe/Madrid"))
+    if now.year != dt.year or now.month != dt.month or now.day != dt.day or now.hour != dt.hour or now.minute != dt.minute:
+        debug_print("Nothing to do here!")
+        exit(0)
 
 def getMap(n_std_tables=4, std_size=80):
-    # Default map, to be adjusted if not all tables have the same size and are totally empty
-    dimensions = {}
-    for i in range(1, n_std_tables+1):
-        dimensions[i] = table(std_size)  # All standard tables have, at least, this number of (free) seats
-    # If a pre-reserved, custom, table must be defined it will go here #
-    return dimensions
-
-
-def getPreference(mapa, n_seats):
-    # We assume we have 50-50 odd and even number groups
-    # We prioritise grouping them as it is better for having people from your group closer
-    # e.g. a group of 4 people next to a group of 6 people will be facing someone from their group
-    #      but if you sit a group of 3 people in between the third group will have 2 people sitting
-    #      next to someone from other group
-    # Best effort: We will have at least two dedicated odd/even tables, then the rest.
-    ids = set(mapa.keys())
-    preference = []
-    fav = 1 if n_seats % 2 == 1 else 2
-    preference.append(fav)
-    ids.remove(fav)
-    for i in ids:
-        preference.append(i)
-    return preference
-
+    return {i: Table(std_size) for i in range(1, n_std_tables + 1)}
 
 def getAllocation(cursor, mapa, gid, n_seats):
-    # Assigns the given number of seats to the given group, by preference
-    preference = getPreference(mapa, n_seats)
-    for i in range(len(preference)):
-        # We assume there MUST be slots for all members, if not they'll stay unassigned
-        t = mapa[preference[i]]
+    for tid, t in mapa.items():
         if t.getAvailable() >= n_seats:
             seat = t.setReservation(n_seats)
-            # Update MySQL database
-            cursor.execute("UPDATE `grupos` SET mesa=%s, asiento=%s WHERE gid = %s", (preference[i], seat, gid))
-            # Generate CSS for group
-            Path(SITE_PATH + '/css/groups/').mkdir(parents=True, exist_ok=True)
-            setCSS(gid, preference[i], seat, n_seats)
-            return preference[i]
+            cursor.execute("UPDATE `grupos` SET mesa=%s, asiento=%s WHERE gid = %s", (tid, seat, gid))
+            return tid, seat
 
-
-def setCSS(gid, tid, first_seat, n_seats):
-    # Generates the CSS for a group
-    f = open(f'{SITE_PATH}/css/groups/{gid}.css', 'w')
-    for seat in range(first_seat, first_seat + n_seats):
-        f.write("td.m" + str(tid) + "a" + str(seat) + " { color: white; background-color: red; }\n")
-    f.close()
-
+def setGroupCSS(gid, tid, first_seat, n):
+    with open(f'{SITE_PATH}/css/groups/{gid}.css', 'w') as f:
+        for i in range(n):
+            seat = first_seat + i
+            f.write(f".m{tid}a{seat} {{ background-color: red; color: white; }}\n")
 
 def setHTML(mapa):
-    from tabulate import tabulate
-    from bs4 import BeautifulSoup
-    import re
-    f = open(f'{SITE_PATH}/mapa.html', 'w')
-    f.write("<table style=\"width: 100%;\">")
-    for n in range(1, len(mapa) + 1):
-        f.write("<tr><td>Mesa " + str(n) + "</td><td>")
-        mesa = tabulate([list(range(1, mapa[n].getSize()+1, 2)), list(range(2, mapa[n].getSize()+1, 2))], tablefmt='html')
-        soup = BeautifulSoup(mesa, "html.parser")
-        for cell in soup.find_all("td"):
-            seat = re.sub("[^0-9]", "", str(cell.string))  # Bugfix
-            cell['class'] = "m" + str(n) + "a" + seat
-            cell['style'] = "border: 1px solid black;"
-        f.write(str(soup))
-        f.write("</tr>")
-    f.write("</table>")
-    f.close()
+    with open(f'{SITE_PATH}/mapa.html', 'w') as f:
+        f.write("<html><head><title>Mapa de asignaciones</title></head><body>")
+        f.write("<div class='mesa-container'>")
 
+        for n, table in mapa.items():
+            f.write(f"<div class='mesa' id='mesa{n}'>")
+            f.write(f"<div class='mesa-title'>Mesa {n}</div>")
+            seats = [f"<div class='asiento m{n}a{s}'>{s}</div>" for s in range(1, table.getSize()+1)]
+            row1, row2 = seats[::2], seats[1::2]
+            for seat1, seat2 in zip(row1, row2):
+                f.write(seat1 + seat2)
+            if len(row1) > len(row2):
+                f.write(row1[-1])
+            f.write("</div>")
+
+        f.write("</div></body></html>")
+
+def getGroups(cursor):
+    # Only groups with 2 or more members are considered as valid
+    # Sorted (desc) by member number but even before odd-member groups
+    cursor.execute("DELETE FROM `grupos` WHERE gid IN (SELECT gid FROM `invitaciones` GROUP BY gid HAVING count(*) = 1)")
+    cursor.execute("SELECT gid, COUNT(*) as count FROM `invitaciones` WHERE gid IS NOT NULL GROUP BY gid HAVING count > 1 ORDER BY count DESC")
+    groups = [[], []]
+    for row in cursor.fetchall():
+        groups[row[1] % 2].append(row)
+    return groups[0] + groups[1]
 
 creds = getMySQLCredentials()
 conn = mysql.connector.connect(user=creds['username'], password=creds['password'], database=creds['database'])
@@ -162,16 +120,12 @@ cursor = conn.cursor()
 force = getGreenlight()
 mapa = getMap(4, 80)
 
-# Delete all groups with 1 member only
-cursor.execute("DELETE FROM `grupos` WHERE gid IN (SELECT gid FROM `invitaciones` GROUP BY gid HAVING count(*) = 1)")
-# Get group IDs and member count for each of them
-cursor.execute("SELECT gid, COUNT(*) as count FROM `invitaciones` WHERE gid IS NOT NULL GROUP BY gid HAVING count > 1 ORDER BY count DESC")
-rows = cursor.fetchall()
-for gid, n in rows:
-    a = getAllocation(cursor, mapa, gid, n)
-    print(f"Group #{n} ({gid}) has been assigned to table {a}")
+# Allocate seats for all groups
+groups = getGroups(cursor)
+for idx, (gid, n) in enumerate(groups):
+    tid, first_seat = getAllocation(cursor, mapa, gid, n)
+    setGroupCSS(gid, tid, first_seat, n)
 
-# Generate HTML map of tables
 setHTML(mapa)
 
 if force:
