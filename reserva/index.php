5958a9a06b840b1c448bd4045b86043f3b22905f
Eloy Gil Guerrero <eloy@eloygil.com>
2025-05-05 15:46:01 +0000

    Improve WhatsApp preview text

--- reserva/index.php
+++ reserva/index.php
@@ -11,7 +11,7 @@ require('settings.php');
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<title><?php echo getEventName(); ?> - Peña "El Follón" <?php echo getEventYear(); ?></title>
+<title>Reserva tu asiento en la <?php echo getEventName(); ?> - Peña "El Follón" <?php echo getEventYear(); ?></title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
